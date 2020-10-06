5d2241e08d9ace850102937d1f82138c95a73cf3
Mojtaba Abedi <68202630+s-mojtaba-a@users.noreply.github.com>
2020-10-06 19:13:39 +0330

    Update python.py

--- python.py
+++ python.py
@@ -13,3 +13,18 @@ if __name__ == '__main__':
     # s[0] = fib(100000) , s[1]=fib(100001)
     print(s[0])
 
+// c++ implemenation
+
+
+pair<ll, ll> fib (int n) {
+    if (n == 0)
+        return {0, 1};
+
+    auto p = fib(n >> 1);
+    ll c = p.first * (2 * p.second - p.first);
+    ll d = p.first * p.first + p.second * p.second;
+    if (n & 1)
+        return {d, c + d};
+    else
+        return {c, d};
+}
